c30d2891f470aeaf346876c7c0092d6fd19ed7cd
AliTaee <ali.taee.72@gmail.com>
2024-06-22 12:00:35 +0200

    practice debug ui

--- src/script.js
+++ src/script.js
@@ -1,14 +1,22 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import gsap from 'gsap'
 
 /**
- * Settings Elements
+ * Debug
  */
-const colorInput = document.getElementById('cubeColor')
-const animationInputs = document.querySelectorAll(
-  'input[name="animation_type"]'
-)
-let animationType = 'none'
+const gui = new GUI({
+  width: 300,
+  title: 'Debug UI',
+})
+const debugObj = {}
+
+window.addEventListener('keydown', (event) => {
+  if (event.key == 'h') {
+    gui.show(gui._hidden)
+  }
+})
 
 /**
  * Base
@@ -22,10 +30,46 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x17d372 })
+debugObj.color = '#33cc59'
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({
+  color: debugObj.color,
+  wireframe: true,
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+gui.add(mesh.position, 'x').min(-3).max(3).step(0.01)
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01)
+gui.add(mesh.position, 'z').min(-3).max(3).step(0.01)
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+gui
+  .addColor(debugObj, 'color')
+  .onChange(() => material.color.set(debugObj.color))
+
+debugObj.spin = () => {
+  gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 })
+}
+gui.add(debugObj, 'spin')
+
+debugObj.subdivision = 2
+gui
+  .add(debugObj, 'subdivision')
+  .min(1)
+  .max(10)
+  .step(1)
+  .onFinishChange(() => {
+    mesh.geometry.dispose()
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObj.subdivision,
+      debugObj.subdivision,
+      debugObj.subdivision
+    )
+  })
 
 /**
  * Sizes
@@ -34,42 +78,19 @@ const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
+
 window.addEventListener('resize', () => {
+  // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
 
-  // Update the camera
+  // Update camera
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
 
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
-
-  // If user move his/her window to another screen it going to be adjusted
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-// Fullscreen
-window.addEventListener('dblclick', () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen()
-  } else {
-    document.exitFullscreen()
-  }
-})
-
-/*
- * Settings
- */
-colorInput.addEventListener('change', (event) => {
-  const newColor = event.target.value
-  mesh.material.color.set(newColor)
-})
-
-animationInputs.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    animationType = event.target.value
-  })
 })
 
 /**
@@ -82,7 +103,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 )
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
@@ -96,6 +119,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
@@ -104,14 +128,6 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
-  const animations = {
-    none: mesh.rotation.y,
-    simple: elapsedTime,
-    sin: Math.sin(elapsedTime),
-    cos: Math.cos(elapsedTime),
-  }
-
-  mesh.rotation.y = animations[animationType]
 
   // Update controls
   controls.update()
